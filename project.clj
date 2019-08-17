ab4fe0858c255f07bb6aa0d7cd94b42cdcf575fc
John Shaffer <jshaffer2112@gmail.com>
2019-08-17 01:42:42 -0500

    Use MIT License.

--- project.clj
+++ project.clj
@@ -1,10 +1,9 @@
 (defproject manila-john "0.1.0-SNAPSHOT"
   :description "A CouchDB library which implements only what clutch does not."
   :url "https://github.com/dunklerfox/manila-john"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "The MIT License"
+            :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [com.ashafa/clutch "0.3.0"]
                  [com.cemerick/url "0.1.0"]]
-
   :main manila-john)
