9f1ebecd3a865ed73fd7fa53bf4a42c3296f0a7e
John Shaffer <jshaffer2112@gmail.com>
2019-08-17 03:07:30 -0500

    Add clutch's dependencies with up-to-date versions.

--- project.clj
+++ project.clj
@@ -4,6 +4,11 @@
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.10.1"]
+                 [cheshire "5.9.0"]
+                 [clj-http "3.10.0"]
                  [com.ashafa/clutch "0.4.0"]
-                 [com.arohner/uri "0.1.2"]]
-  :main manila-john)
+                 [com.arohner/uri "0.1.2"]
+                 [commons-codec "1.13"]
+                 [org.clojure/clojurescript "1.10.520" :optional true]]
+  :main manila-john
+  :min-lein-version "2.0.0")
