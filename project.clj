08b70149042345f34bcecedc1a25540c7235b9cc
John Shaffer <jshaffer2112@gmail.com>
2019-08-17 01:48:25 -0500

    Change project URL.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject manila-john "0.1.0-SNAPSHOT"
   :description "A CouchDB library which implements only what clutch does not."
-  :url "https://github.com/dunklerfox/manila-john"
+  :url "https://github.com/john-shaffeR/manila-john"
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.6.0"]
